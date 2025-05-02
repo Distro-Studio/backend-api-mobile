bffa00094ee12048b30ad48bf6b0c8537cb59d0b
rezahawari <rezahawari19@gmail.com>
2025-05-02 09:22:56 +0700

    fixing

--- app/Http/Controllers/JadwalController.php
+++ app/Http/Controllers/JadwalController.php
@@ -60,31 +60,32 @@ class JadwalController extends Controller
             })
             ->with('shift')
             ->first();
+            if($jadwal){
+                if(Carbon::parse($jadwal->tgl_keluar)->isToday()) {
+                    $jwl = Jadwal::where('user_id', Auth::user()->id)
+                        ->where(function ($query) use ($today, $yesterday, $now) {
+                            // Kondisi 1: Jadwal hari ini
+                            $query->whereDate('tgl_mulai', $today);
+                            $query->where('shift_id', '!=', 0);
+                            // Kondisi 2: Shift malam (misalnya mulai kemarin dan selesai hari ini)
+                            $query->orWhere(function ($query) use ($today, $yesterday, $now) {
+                                $query->whereDate('tgl_mulai', $today)
+                                    ->whereHas('presensi', function($shiftQuerys) use ($now) {
+                                        $shiftQuerys->whereNull('jam_keluar');
+                                    });
+                            });
+                        })
+                        ->with('shift')
+                        ->first();
+                    if($jwl){
+                        $times = date('Y-m-d H:i:s');
+                        $nowTimes = Carbon::parse($times);
+                        $schDates = Carbon::parse($jwl->tgl_mulai . ' ' . $jwl->shift->jam_from);
+                        $durations = $schDates->diffInSeconds($nowTimes);
 
-            if(Carbon::parse($jadwal->tgl_keluar)->isToday()) {
-                $jwl = Jadwal::where('user_id', Auth::user()->id)
-                    ->where(function ($query) use ($today, $yesterday, $now) {
-                        // Kondisi 1: Jadwal hari ini
-                        $query->whereDate('tgl_mulai', $today);
-                        $query->where('shift_id', '!=', 0);
-                        // Kondisi 2: Shift malam (misalnya mulai kemarin dan selesai hari ini)
-                        $query->orWhere(function ($query) use ($today, $yesterday, $now) {
-                            $query->whereDate('tgl_mulai', $today)
-                                ->whereHas('presensi', function($shiftQuerys) use ($now) {
-                                    $shiftQuerys->whereNull('jam_keluar');
-                                });
-                        });
-                    })
-                    ->with('shift')
-                    ->first();
-                if($jwl){
-                    $times = date('Y-m-d H:i:s');
-                    $nowTimes = Carbon::parse($times);
-                    $schDates = Carbon::parse($jwl->tgl_mulai . ' ' . $jwl->shift->jam_from);
-                    $durations = $schDates->diffInSeconds($nowTimes);
-
-                    if ($durations < 7200) {
-                        $jadwal = $jwl;
+                        if ($durations < 7200) {
+                            $jadwal = $jwl;
+                        }
                     }
                 }
             }
